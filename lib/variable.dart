ca4a7fe777b83a394e8c7cd32c4af50e040087ee
Satyam1Vishwakarma <97113954+Satyam1Vishwakarma@users.noreply.github.com>
2023-07-27 06:44:43 +0530

    Update variable.dart

--- lib/variable.dart
+++ lib/variable.dart
@@ -1,5 +1,5 @@
-String baseurl = 'https://backendflutter.herokuapp.com';
-String wsurl = 'wss://backendflutter.herokuapp.com/ws';
+String baseurl = 'https://backend-o17i.onrender.com';
+String wsurl = 'wss://backend-o17i.onrender.com/ws';
 String getavatar = baseurl + '/getavatar';
 String getusers = baseurl + '/getusers';
 String postavatar = baseurl + '/postavatar';
